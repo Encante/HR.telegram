22e5ef64d21fab2d7c6cb3986ee54b801ac10a23
michal.gladysiak <encatee@gmail.com>
2022-11-16 17:47:57 +0100

    work in progress objekty 2

--- src/main/java/net/ddns/encante/telegram/HR/WebhookUpdate.java
+++ src/main/java/net/ddns/encante/telegram/HR/WebhookUpdate.java
@@ -1,6 +1,8 @@
 package net.ddns.encante.telegram.HR;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.experimental.FieldDefaults;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,31 +11,32 @@ import java.util.List;
 @Getter
 public class WebhookUpdate {
 
-        private Long update_id;
+        Long update_id;
         Message message;
     }
     @Getter
+    @FieldDefaults(level = AccessLevel.PRIVATE)
     class Message {
-        private Long message_id;
-        private Integer message_thread_id;
+        Long message_id;
+        Integer message_thread_id;
         User from;
         Chat sender_chat;
-        private Long date;
+        Long date;
         Chat chat;
         User forward_from_chat;
-        private Long forward_from_message_id;
-        private String forward_signature;
-        private String forward_sender_name;
-        private Long forward_date;
-        private boolean is_topic_message;
-        private boolean is_automatic_forward;
+        Long forward_from_message_id;
+        String forward_signature;
+        String forward_sender_name;
+        Long forward_date;
+        boolean is_topic_message;
+        boolean is_automatic_forward;
         Message reply_to_message;
         User via_bot;
-        private Long edit_date;
-        private boolean has_protected_content;
-        private String media_group_id;
-        private String author_signature;
-        private String text;
+        Long edit_date;
+        boolean has_protected_content;
+        String media_group_id;
+        String author_signature;
+        String text;
         ArrayList <MessageEntity> entities;
         Animation animation;
         Audio audio;
@@ -43,7 +46,7 @@ public class WebhookUpdate {
         Video video;
         VideoNote video_note;
         Voice voice;
-        private String caption;
+        String caption;
         ArrayList<MessageEntity> caption_entities;
         Contact contact;
         Dice dice;
@@ -53,19 +56,19 @@ public class WebhookUpdate {
         Location location;
         ArrayList<User> new_chat_members;
         User left_chat_member;
-        private String new_chat_title;
+        String new_chat_title;
         ArrayList<PhotoSize> new_chat_photo;
-        private boolean delete_chat_photo;
-        private boolean group_chat_created;
-        private boolean supergroup_chat_created;
-        private boolean channel_chat_created;
+        boolean delete_chat_photo;
+        boolean group_chat_created;
+        boolean supergroup_chat_created;
+        boolean channel_chat_created;
         MessageAutoDeleteTimerChanged message_auto_delete_timer_changed;
-        private Long migrate_to_chat_id;
-        private Long migrate_from_chat_id;
+        Long migrate_to_chat_id;
+        Long migrate_from_chat_id;
         Message pinned_message;
         Invoice invoice;
         SuccessfulPayment successful_payment;
-        private String connected_website;
+        String connected_website;
         PassportData passport_data;
         ProximityAlertTriggered proximity_alert_triggered;
         ForumTopicCreated forum_topic_created;
@@ -80,18 +83,79 @@ public class WebhookUpdate {
     }
 
     @Getter
-    class Chat {
-        private Long id;
-        private String first_name;
-        private String last_name;
-        private String type;
+    @FieldDefaults(level = AccessLevel.PRIVATE)
+    class MessageId{
+    Long message_id;
     }
 
     @Getter
+    @FieldDefaults(level = AccessLevel.PRIVATE)
+    class MessageEntity{
+    String type;
+    Integer offset;
+    Integer lenght;
+    String url;
+    User user;
+    String language;
+    String custom_emoji_id;
+    }
+
+    @Getter
+    @FieldDefaults(level = AccessLevel.PRIVATE)
+    class PhotoSize{
+    String file_id;
+    String file_unique_id;
+    Integer width;
+    Integer height;
+    Long file_size;
+    }
+
+    @Getter
+    @FieldDefaults(level = AccessLevel.PRIVATE)
+    class Animation{
+    String file_id;
+    String file_unique_id;
+    Integer width;
+    Integer height;
+    Integer duration;
+    PhotoSize thumb;
+    String file_name;
+    String mime_type;
+    Long file_size;
+    }
+    @Getter
+    @FieldDefaults(level = AccessLevel.PRIVATE)
+    class Audio{
+    String file_id;
+    String file_unique_id;
+    Integer duration;
+    String performer;
+    String title;
+    String file_name;
+    String mime_type;
+    Long file_size;
+    PhotoSize thumb;
+    }
+    @Getter
+    @FieldDefaults(level = AccessLevel.PRIVATE)
+    class Document{
+
+    }
+    @Getter
+    @FieldDefaults(level = AccessLevel.PRIVATE)
+    class Chat {
+        Long id;
+        String first_name;
+        String last_name;
+        String type;
+    }
+
+    @Getter
+    @FieldDefaults(level = AccessLevel.PRIVATE)
     class User {
-        private Long id;
-        private boolean is_bot;
-        private String first_name;
-        private String last_name;
-        private String language_code;
+        Long id;
+        boolean is_bot;
+        String first_name;
+        String last_name;
+        String language_code;
     }
